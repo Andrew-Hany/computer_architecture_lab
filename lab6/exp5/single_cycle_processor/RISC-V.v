145bfe1e395dfd17bcf1bcd23436596799035bd0
Andrew-Hany <andrewhany@aucegypt.edu>
2020-10-19 14:55:32 +0200

    lab 6

---
+++ lab6/exp5/single_cycle_processor/RISC-V.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2020 06:12:48 PM
+// Design Name: 
+// Module Name: RISC-V
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RISC_V( );
+endmodule
